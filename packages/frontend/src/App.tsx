764a0918a83c6c36e0f2197ae12d18b389bc15bb
Mukai.T <127798683+okayus@users.noreply.github.com>
2025-04-29 23:15:08 +0900

    feat: Add MonsterDetailPage route to App

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
+import MonsterDetailPage from './pages/MonsterDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="monsters/:id" element={<MonsterDetailPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
