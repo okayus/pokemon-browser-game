1d2a3a2bdc48b4927abdbc5246b1c11cc6e2e555
Mukai.T <127798683+okayus@users.noreply.github.com>
2025-04-29 23:32:21 +0900

    fix: Update CORS settings to allow localhost and 127.0.0.1

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -19,7 +19,11 @@ app.use(
   '*',
   logger(),
   cors({
-    origin: ['http://localhost:3000', 'https://pokemon-browser-game.pages.dev'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://pokemon-browser-game.pages.dev'
+    ],
     credentials: true,
   })
 );
