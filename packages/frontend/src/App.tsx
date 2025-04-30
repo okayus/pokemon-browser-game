c46cc6334bda64e2f1f6225edd837e611cd12682
Mukai.T <127798683+okayus@users.noreply.github.com>
2025-04-30 22:43:25 +0900

    feat: update App component with AuthProvider and protected routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import MonsterDetailPage from './pages/MonsterDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
 import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
@@ -12,21 +12,17 @@ function App() {
     <AuthProvider>
       <div className="App">
         <Routes>
-          {/* 公開ルート */}
           <Route path="/login" element={<LoginPage />} />
-
-          {/* レイアウト付きルート */}
           <Route path="/" element={<Layout />}>
-            {/* パブリックページ */}
             <Route index element={<HomePage />} />
-
-            {/* 要認証ページ */}
+            <Route path="monsters/:id" element={<MonsterDetailPage />} />
+            
+            {/* 保護されたルート */}
             <Route element={<ProtectedRoute />}>
-              <Route path="monsters/:id" element={<MonsterDetailPage />} />
-              {/* 他の要認証ページをここに追加 */}
+              <Route path="profile" element={<div>プロフィールページ (作成中)</div>} />
+              <Route path="battles" element={<div>バトルページ (作成中)</div>} />
             </Route>
-
-            {/* 404ページ */}
+            
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
